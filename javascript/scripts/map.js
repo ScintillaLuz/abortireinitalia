2206d4ef19a09368583a04d0166ec645a2add467
Anaketa <g.sambrotta@gmail.com>
2015-04-10 16:57:17 +0200

    fix display markers

--- javascript/scripts/map.js
+++ javascript/scripts/map.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+function initialize() {
 
   var myLatlng = new google.maps.LatLng(45.465142,9.189749,5.465142);
   var pharmacyMarkerCoordinate = new google.maps.LatLng(45.484642, 9.187689);
@@ -7,13 +7,12 @@ $( document ).ready(function() {
   center: myLatlng,
   zoom: 10
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   setMarkersHospitals(map, hospitals);
   setMarkersPharmacy(map, pharmacy);
 
-});
+}
 
 var hospitals = [
   ['San Raffaele', 45.506783, 9.265623, 4, 'ciao1'],
@@ -29,20 +28,6 @@ var pharmacy = [
 
 function setMarkersHospitals(map, locations){
 
-  var contentString = hospitals[4];
-  var infowindow = new google.maps.InfoWindow();
-
-  var hospitalMarkerCoordinate = new google.maps.LatLng(hospitals[1], hospitals[2]);
-  var hospitalMarker = '../img/marker-pin-google-hi.png';
-  
-  hospitalMarker = new google.maps.Marker({
-    position: hospitalMarkerCoordinate,
-    map: map,
-    icon: hospitalMarker,
-    title: hospitals[4],
-    zoom: hospitals[3]
-  });
-
   var bindInfoW = function(hospitalMarker, contentString, infowindow){
     google.maps.event.addListener(hospitalMarker, 'mouseover', function() {
       infowindow.setContent(contentString);
@@ -56,6 +41,20 @@ function setMarkersHospitals(map, locations){
 
   for (var i = 0; i < locations.length; i++){
     hospitals = locations[i];
+    var hospitalMarkerCoordinate = new google.maps.LatLng(hospitals[1], hospitals[2]);
+    var hospitalMarker = '../img/marker-pin-google-hi.png';
+    
+    hospitalMarker = new google.maps.Marker({
+      position: hospitalMarkerCoordinate,
+      map: map,
+      icon: hospitalMarker,
+      title: hospitals[4],
+      zoom: hospitals[3]
+    });
+
+    var contentString = hospitals[4];
+    var infowindow = new google.maps.InfoWindow();
+
     bindInfoW(hospitalMarker, contentString, infowindow);
   }
 }
@@ -63,17 +62,6 @@ function setMarkersHospitals(map, locations){
 
 
 var setMarkersPharmacy = function(map, locations){
-  var pharmacyMarkerCoordinate = new google.maps.LatLng(pharmacy[1], pharmacy[2]);
-  
-  var pharmacyMarker = new google.maps.Marker({
-    position: pharmacyMarkerCoordinate,
-    map: map,
-    title: pharmacy[4],
-    zoom: pharmacy[3]
-  });
-
-  var contentString = pharmacy[4];
-  var infowindow = new google.maps.InfoWindow();
 
   var bindInfoW = function(pharmacyMarker, contentString, infowindow){
     google.maps.event.addListener(pharmacyMarker, 'mouseover', function() {
@@ -89,8 +77,20 @@ var setMarkersPharmacy = function(map, locations){
 
   for (var i = 0; i < locations.length; i++){
     pharmacy = locations[i]; 
+    var pharmacyMarkerCoordinate = new google.maps.LatLng(pharmacy[1], pharmacy[2]);
+  
+    var pharmacyMarker = new google.maps.Marker({
+      position: pharmacyMarkerCoordinate,
+      map: map,
+      title: pharmacy[4],
+      zoom: pharmacy[3]
+    });
+
+    var contentString = pharmacy[4];
+    var infowindow = new google.maps.InfoWindow();
+
     bindInfoW(pharmacyMarker, contentString, infowindow);
   }
 };
 
-//google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
